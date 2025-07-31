29aa089cf236e2c528a7db4479674b9037875c83
MINSIK-W <108776722+MINSIK-W@users.noreply.github.com>
2025-07-31 18:25:31 +0900

    Hero 컴포넌트 추가

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header.tsx';
+import Hero from '@/components/Hero.tsx';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
