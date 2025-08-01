80a5f917ae1668a544834ac37657b371f4bf5d12
MINSIK-W <108776722+MINSIK-W@users.noreply.github.com>
2025-08-02 00:42:16 +0900

    한글 누락 부분 데이터 추가 수정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { koKR } from '@clerk/localizations';
-import './index.css';
-import App from './App.tsx';
+import '@/index.css';
+import App from '@/App.tsx';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
+export const customKoKR = {
+  ...koKR,
+  commerce: {
+    ...koKR.commerce,
+    subscribe: '구독하기',
+  },
+};
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} localization={koKR}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} localization={customKoKR}>
       <App />
     </ClerkProvider>
   </StrictMode>
