65e86ba8bc6eb610e6a080c5038e65464668ed36
MINSIK-W <108776722+MINSIK-W@users.noreply.github.com>
2025-08-02 00:43:10 +0900

    ErrorFallback, LoadingFallback 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { Suspense, useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { Router } from '@/router/Router.tsx';
+import { ErrorFallback } from '@/components/common/ErrorFallback.tsx';
+import { LoadingFallback } from '@/components/common/LoadingFallback.tsx';
 
 export default function App() {
   useEffect(() => {
@@ -9,12 +11,12 @@ export default function App() {
   }, []);
   return (
     <ErrorBoundary
-      FallbackComponent={() => <></>}
+      FallbackComponent={() => <ErrorFallback />}
       onError={(error, info) => {
         console.error('오류 발생:', error, info);
       }}
     >
-      <Suspense fallback={<></>}>
+      <Suspense fallback={<LoadingFallback />}>
         <RouterProvider router={Router} />
       </Suspense>
     </ErrorBoundary>
