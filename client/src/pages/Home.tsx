37d98abd55a9f24db16fd911e17d2e76953f1ef4
MINSIK-W <108776722+MINSIK-W@users.noreply.github.com>
2025-08-02 00:43:51 +0900

    AiTools, Testimonial, Plan 컴포넌트 추가

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import Header from '@/components/Header.tsx';
 import Hero from '@/components/Hero.tsx';
+import AiTools from '@/components/AiTools.tsx';
+import Testimonial from '@/components/Testimonial.tsx';
+import Plan from '@/components/Plan.tsx';
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <AiTools />
+      <Testimonial />
+      <Plan />
     </>
   );
 }
