689a1201185129010b86eaf9510a86355705462c
MINSIK-W <108776722+MINSIK-W@users.noreply.github.com>
2025-08-02 01:34:21 +0900

    생각보다 누락된 키가 많아 따로 파일 분리 하여 커스텀

---
+++ client/src/localization/customKoKR.ts
@@ -0,0 +1,43 @@
+import { koKR } from '@clerk/localizations';
+
+export const customKoKR = {
+  ...koKR,
+  commerce: {
+    ...koKR.commerce,
+    subscribe: '구독하기',
+    paymentSource: {
+      ...koKR.commerce?.paymentSource,
+      dev: {
+        ...koKR.commerce?.paymentSource?.dev,
+        testCardInfo: '테스트 카드 정보',
+        developmentMode: '개발 모드',
+        cardNumber: '카드 번호',
+        expirationDate: '만료일',
+        cvcZip: 'CVC,ZIP',
+      },
+    },
+  },
+  userProfile: {
+    ...koKR.userProfile,
+    navbar: {
+      ...koKR.userProfile?.navbar,
+      billing: '결제',
+    },
+    billingPage: {
+      ...koKR.userProfile?.billingPage,
+      title: '결제',
+      subscriptionsListSection: {
+        ...koKR.userProfile?.billingPage?.subscriptionsListSection,
+        title: '내 플랜',
+        actionLabel__switchPlan: '요금제 전환',
+      },
+      paymentSourcesSection: {
+        ...koKR.userProfile?.billingPage?.paymentSourcesSection,
+        title: '결제 방법',
+        add: '새 결제 방법 추가',
+        addSubtitle: '계정에 새 결제 방법을 추가하세요.',
+        formButtonPrimary__add: '결제 방법 추가',
+      },
+    },
+  },
+};
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ClerkProvider } from '@clerk/clerk-react';
-import { koKR } from '@clerk/localizations';
 import '@/index.css';
 import App from '@/App.tsx';
+import { customKoKR } from '@/localization/customKoKR.ts';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-export const customKoKR = {
-  ...koKR,
-  commerce: {
-    ...koKR.commerce,
-    subscribe: '구독하기',
-  },
-};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} localization={customKoKR}>
