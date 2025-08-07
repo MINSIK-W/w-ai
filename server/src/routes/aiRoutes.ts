cd0b17e23c3abb4d461bd5d025f9d6fe9cce8f6a
MINSIK-W <108776722+MINSIK-W@users.noreply.github.com>
2025-08-08 00:03:03 +0900

    서버 라우트 설정

---
+++ server/src/routes/aiRoutes.ts
@@ -0,0 +1,22 @@
+import express, { Router } from 'express';
+import { auth } from '@/middlewares/auth';
+import {
+  article,
+  background,
+  blogTitle,
+  images,
+  object,
+  resumes,
+} from '@/controllers/aiController';
+import { upload } from '@/configs/multer';
+
+const aiRouter: Router = express.Router();
+
+aiRouter.post('/article', auth, article);
+aiRouter.post('/title', auth, blogTitle);
+aiRouter.post('/images', auth, images);
+aiRouter.post('/background', upload.single('image'), auth, background);
+aiRouter.post('/object', upload.single('image'), auth, object);
+aiRouter.post('/resumes', upload.single('resume'), auth, resumes);
+
+export default aiRouter;
---
+++ server/src/routes/userRoutes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { auth } from '@/middlewares/auth';
+import {
+  getPublishedCreations,
+  getUserCreations,
+  toggleLikeCreation,
+} from '@/controllers/userController';
+
+const userRoutes = express();
+
+userRoutes.get('/getUserCreations', auth, getUserCreations);
+userRoutes.get('/getPublishedCreations', auth, getPublishedCreations);
+userRoutes.post('/toggleLikeCreation', auth, toggleLikeCreation);
+
+export default userRoutes;
